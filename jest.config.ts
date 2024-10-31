f4dc73fc7a8696e6a52076f0edd16962ce6d5283
Jung Yoonho <hoejy0821@gmail.com>
2024-10-31 14:49:41 +0900

    [#17] 📦️ Jest 기초 셋업 및 예시 테스트 코드 작성 (#18)
    * [#17] 📦️ add jest related packages
    
    * [#17] 📦️ add more jest related packages
    
    * [#17] 📦️ jest config setup
    
    * [#17] 📦️ tsconfig jest setup
    
    * [#17] 🔧 jest scripts setup
    
    * [#17] 📦️ add ts-node into dev-dependencies
    
    * [#17] 📦️ update packages
    
    * [#17] 🔧 update jest-related config
    
    * [#17] ✅ add initial Button test code

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
+
+export default config;
