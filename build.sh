e16c614d3c4762c81a89e9ab24981112aea3bf1b
Jung Yoonho <hoejy0821@gmail.com>
2024-11-05 16:28:13 +0900

    [#59] 🔧 깃헙 액션 관련 파일 (.yml, build.sh) 수정 (#70)
    * [#59] 🔧 update git-push.yml file
    
    * [#59] 🔧 update build.sh file

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-cd ../
+
+# 최상위 디렉토리로 이동
+cd "$(dirname "$0")"/..
+
+# output 디렉토리 생성 (이미 존재하면 삭제 후 생성)
+rm -rf output
 mkdir output
+
+# frontend 폴더 내 모든 파일을 output으로 복사
 cp -R ./frontend/* ./output
-cp -R ./output ./frontend/
+
+# 확인을 위해 echo 메시지 추가
+echo "Files copied to output directory."
