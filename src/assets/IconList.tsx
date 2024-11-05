bfcfdc088e971a4d70638e1b988e3811c345253b
yongb2n <144973656+yongb2n@users.noreply.github.com>
2024-11-05 16:24:35 +0900

    [#43] ✨ TextInput 컴포넌트 구현 (#68)
    * [#43]✨ Implement TextInput component
    
    * [#43] ♻️ Refactor TextInput with tailwind-merge and update baseStyles
    
    * [#43] ✨ Create IconList component with svg icons
    
    * [#43] 💄 Add essential styles for button, input and select in globals.css

---
+++ src/assets/IconList.tsx
@@ -0,0 +1,73 @@
+import IcArrowRight from './icons/ic-arrow-right.svg'
+import IcAvatar from './icons/ic-avatar.svg'
+import IcBin from './icons/ic-bin.svg'
+import IcCalendar from './icons/ic-calendar.svg'
+import IcCaretDown from './icons/ic-caret-down.svg'
+import IcCaretUp from './icons/ic-caret-up.svg'
+import IcCheck from './icons/ic-check.svg'
+import IcChevronLeft from './icons/ic-chevron-left.svg'
+import IcChevronRight from './icons/ic-chevron-right.svg'
+import IcClose from './icons/ic-close.svg'
+import IcComment from './icons/ic-comment.svg'
+import IcEdit from './icons/ic-edit.svg'
+import IcEditorAlignCenter from './icons/ic-editor-align-center.svg'
+import IcEditorAlignLeft from './icons/ic-editor-align-left.svg'
+import IcEditorAlignRight from './icons/ic-editor-align-right.svg'
+import IcEditorCode from './icons/ic-editor-code.svg'
+import IcEditorFontBold from './icons/ic-editor-font-bold.svg'
+import IcEditorFontSize from './icons/ic-editor-font-size.svg'
+import IcEditorImage from './icons/ic-editor-image.svg'
+import IcEditorItalic from './icons/ic-editor-italic.svg'
+import IcEditorQuote from './icons/ic-editor-quote.svg'
+import IcEditorStrike from './icons/ic-editor-strike.svg'
+import IcEditorSymbol from './icons/ic-editor-symbol.svg'
+import IcEditorUnderline from './icons/ic-editor-underline.svg'
+import IcEyeClosed from './icons/ic-eye-closed.svg'
+import IcEyeOpen from './icons/ic-eye-open.svg'
+import IcHeart from './icons/ic-heart.svg'
+import IcKebabMenu from './icons/ic-kebab-menu.svg'
+import IcMemberAdd from './icons/ic-member-add.svg'
+import IcMemberDelete from './icons/ic-member-delete.svg'
+import IcPaperPlane from './icons/ic-paper-plane.svg'
+import IcPlus from './icons/ic-plus.svg'
+import IcSearch from './icons/ic-search.svg'
+import IcShare from './icons/ic-share.svg'
+import IcStart from './icons/ic-start.svg'
+
+export {
+  IcArrowRight,
+  IcAvatar,
+  IcBin,
+  IcCalendar,
+  IcCaretDown,
+  IcCaretUp,
+  IcCheck,
+  IcChevronLeft,
+  IcChevronRight,
+  IcClose,
+  IcComment,
+  IcEdit,
+  IcEditorAlignCenter,
+  IcEditorAlignLeft,
+  IcEditorAlignRight,
+  IcEditorCode,
+  IcEditorFontBold,
+  IcEditorFontSize,
+  IcEditorImage,
+  IcEditorItalic,
+  IcEditorQuote,
+  IcEditorStrike,
+  IcEditorSymbol,
+  IcEditorUnderline,
+  IcEyeClosed,
+  IcEyeOpen,
+  IcHeart,
+  IcKebabMenu,
+  IcMemberAdd,
+  IcMemberDelete,
+  IcPaperPlane,
+  IcPlus,
+  IcSearch,
+  IcShare,
+  IcStart,
+}
---
+++ src/components/common/input/TextInput.tsx
@@ -0,0 +1,53 @@
+import clsx from 'clsx'
+import { ReactNode } from 'react'
+import { FieldValues, UseFormRegister } from 'react-hook-form'
+import { twMerge } from 'tailwind-merge'
+
+interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean
+  register?: ReturnType<UseFormRegister<FieldValues>>
+  startAdornment?: ReactNode
+  endAdornment?: ReactNode
+  className?: string
+  fullWidth?: boolean
+}
+
+const baseStyles =
+  'h-52 p-14 focus:border-primary-normal rounded-lg border-1 border-gray-200 text-body2 leading-body2 font-medium placeholder:text-gray-500'
+const errorStyles = 'border-semantic-negative'
+
+export const TextInput = ({
+  type = 'text',
+  error = false,
+  register,
+  startAdornment = '',
+  endAdornment = '',
+  className = '',
+  fullWidth = false,
+  ...props
+}: TextInputProps): JSX.Element => {
+  const textInputStyle = twMerge(
+    baseStyles,
+    clsx(
+      {
+        [errorStyles]: error,
+        'w-full': fullWidth,
+        'pl-48': startAdornment,
+        'pr-48': endAdornment,
+      },
+      className
+    )
+  )
+
+  return (
+    <div className='relative'>
+      {startAdornment && (
+        <span className='absolute left-14 top-10'>{startAdornment}</span>
+      )}
+      <input {...register} type={type} className={textInputStyle} {...props} />
+      {endAdornment && (
+        <span className='absolute right-14 top-14'>{endAdornment}</span>
+      )}
+    </div>
+  )
+}
